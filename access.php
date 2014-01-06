f1f9936fd76fed1851c16dc9a754ed8a15f1070b
leftyfb <leftyfb@left-click.org>
2014-01-05 23:50:17 -0500

    Working hours updates

--- access.php
+++ access.php
@@ -21,10 +21,10 @@ $page_count = (int)ceil($row_count / $limit - 1);
 
 if(isset($_GET['name']) && !empty($_GET['name']))
  {$name = ($_GET['name']);
- $result = mysqli_query($con,"SELECT NodeName,Timestamp, Name FROM EntryLog, AccessCards WHERE EntryLog.CardID = AccessCards.CardID AND AccessCards.Name = '$name'");
+ $result = mysqli_query($con,"SELECT * FROM EntryLog, AccessCards WHERE EntryLog.CardID = AccessCards.CardID AND AccessCards.Name = '$name'");
  $row_count = mysqli_num_rows($result);
  $page_count = (int)ceil($row_count / $limit - 1);
- $result = mysqli_query($con,"SELECT NodeName,Timestamp, Name FROM EntryLog, AccessCards WHERE EntryLog.CardID = AccessCards.CardID AND AccessCards.Name = '$name' ORDER BY Timestamp DESC LIMIT {$start},{$limit}");}
+ $result = mysqli_query($con,"SELECT * FROM EntryLog, AccessCards WHERE EntryLog.CardID = AccessCards.CardID AND AccessCards.Name = '$name' ORDER BY Timestamp DESC LIMIT {$start},{$limit}");}
 elseif(isset($_GET['edit']) && !empty($_GET['edit']))
  {
   $edit = ($_GET['edit']);
@@ -53,7 +53,7 @@ elseif(isset($_GET['edit']) && !empty($_GET['edit']))
     echo "<td>" . $x . "PM</td>";
    }
    echo "</tr>";
-   $result = mysqli_query($con,"SELECT * from AccessNodes WHERE CardID = '$CardID'");
+   $result = mysqli_query($con,"SELECT * FROM AccessNodes, AccessCards WHERE AccessNodes.CardID = AccessCards.CardID AND AccessNodes.CardID = '$CardID'");
    while($row = mysqli_fetch_array($result)) 
    {
     echo "<form action=\"log.php\" method=\"post\">";
@@ -61,9 +61,20 @@ elseif(isset($_GET['edit']) && !empty($_GET['edit']))
     echo "<td>" . $row['NodeName'] . "</td>";
     echo "<td>" . $row['Location'] . "</td>";
     for ($x=0; $x<=23; $x++)
-   {
-   echo "<td><input type=\"checkbox\" name=\"Hour" . str_pad($x,2,"0",STR_PAD_LEFT) . "\" value=\"" . $row['Hour00'] . "\"></td>";
+    {
+     $hournum = str_pad($x,2,"0",STR_PAD_LEFT);
+     $hour = "Hour" . $hournum;
+     if($row[$hour] == "1") 
+     {echo "<td><input type=\"checkbox\" name=\"" . $hour . "\" value=\"1\" checked></td>";}
+     else
+     {echo "<td><input type=\"checkbox\" name=\"" . $hour . "\" value=\"0\"></td>";}
    }
+    $NodeName = $row['NodeName'];
+    $Name = $row['Name'];
+    $CardID = $row['CardID'];
+    echo "<input type=\"hidden\" name=\"NodeName\" value=\"$NodeName\">";
+    echo "<input type=\"hidden\" name=\"Name\" value=\"$Name\">";
+    echo "<input type=\"hidden\" name=\"CardID\" value=\"$CardID\">";
     echo "<td><input type=\"submit\" value=\"Update\"></td></tr>";
     echo "</form>";
    }
@@ -82,9 +93,28 @@ elseif(isset($_POST['CardID']) && isset($_POST['Name']) && isset($_POST['Tone'])
  echo "<a href=" . $_SERVER['SCRIPT_NAME'] . "?edit=" . urlencode($Name) . ">Return to editing</a>";
  exit();
  }
-
+elseif(isset($_POST['NodeName']))
+  {
+   $NodeName = $_POST['NodeName'];
+   $Name = $_POST['Name'];
+   $CardID = $_POST['CardID'];
+   echo "<a href=" . $_SERVER['SCRIPT_NAME'] . ">Home</a><br>";
+   for ($x=0; $x<=23; $x++)
+   {
+    $hournum = str_pad($x,2,"0",STR_PAD_LEFT);
+    $hour = "Hour" . $hournum;
+    if(isset($_POST[$hour]))
+     {$houraccess = 1;}
+    elseif(empty($_POST[$hour]))
+     {$houraccess = 0;}
+      mysqli_query($con,"UPDATE AccessNodes SET $hour='$houraccess' WHERE NodeName = '$NodeName' AND CardID = '$CardID'");
+    echo "<a>Updated $hour with $houraccess for $Name on $NodeName<a/><br>";
+   }
+    echo "<a href=" . $_SERVER['SCRIPT_NAME'] . "?edit=" . urlencode($Name) . ">Return to editing</a>";
+    exit();
+  }
 else
-  {$result = mysqli_query($con,"SELECT NodeName,Timestamp, Name FROM EntryLog, AccessCards WHERE EntryLog.CardID = AccessCards.CardID ORDER BY Timestamp DESC LIMIT {$start},{$limit}");}
+  {$result = mysqli_query($con,"SELECT * FROM EntryLog, AccessCards WHERE EntryLog.CardID = AccessCards.CardID ORDER BY Timestamp DESC LIMIT {$start},{$limit}");}
 
 echo "<style type=\"text/css\">";
 echo "tbody tr:nth-child(odd){ background-color:#ccc; }";
@@ -95,6 +125,7 @@ echo "<table border='0'>
 <th>Node Name</th>
 <th>Timestamp</th>
 <th>Name</th>
+<th>Result</th>
 </tr>";
 
 while($row = mysqli_fetch_array($result))
@@ -103,6 +134,7 @@ while($row = mysqli_fetch_array($result))
   echo "<td>" . $row['NodeName'] . "</td>";
   echo "<td>" . $row['Timestamp'] . "</td>";
   echo "<td><a href=" . $_SERVER['SCRIPT_NAME'] . "?name=" . urlencode($row['Name']) . ">" . $row['Name'] . "</a><a href=" . $_SERVER['SCRIPT_NAME'] . "?edit=" . urlencode($row['Name']) . "><img align=right src=edit.png></img></a></td>";
+  echo "<td>" . $row['Result'] . "</td>";
   echo "</tr>";
   }
 if($page == "0")
@@ -113,13 +145,13 @@ else
  else
   {echo "<tr><td><a href=" . $_SERVER['SCRIPT_NAME'] . "?page=$prevpage>Prev $limit</a></td>";}
 
-echo "<td>&nbsp</td>";
+echo "<td>&nbsp</td><td>&nbsp</td>";
 
 if($page < $page_count)
  if(isset($_GET['name']) && !empty($_GET['name']))
-  {echo "<td><a href=" . $_SERVER['SCRIPT_NAME'] . "?page=$nextpage&name=" . urlencode($name) . ">Next $limit</a></td></tr>";}
+  {echo "<td align=\"right\"><a href=" . $_SERVER['SCRIPT_NAME'] . "?page=$nextpage&name=" . urlencode($name) . ">Next $limit</a></td></tr>";}
  else
-  {echo "<td><a href=" . $_SERVER['SCRIPT_NAME'] . "?page=$nextpage>Next $limit</a></td></tr>";}
+  {echo "<td align=\"right\"><a href=" . $_SERVER['SCRIPT_NAME'] . "?page=$nextpage>Next $limit</a></td></tr>";}
 else
  {echo "<td>&nbsp</td>"; } 
 echo "</table>";
